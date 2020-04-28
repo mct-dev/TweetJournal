0d08ba20f4906952ffef1ca38629b901da7b4557
Michael Tobias <michael@bitcoinira.com>
2020-04-27 19:56:12 -0700

    adds code coverage exclude to migrations

--- TweetJournal.Access.Entries/Migrations/20200413152238_InitialCreate.cs
+++ TweetJournal.Access.Entries/Migrations/20200413152238_InitialCreate.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace TweetJournal.Access.Entries.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class InitialCreate : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
