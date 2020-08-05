3799b70326d8b1897e293955e03fc7061d3fbda6
Michael Tobias <michael@bitcoinira.com>
2020-08-05 10:58:00 -0700

    Add tailwind css, remove default stylesheets and fonts, add entry list and input components

---
+++ TweetJournal.Web/Models/Entry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TweetJournal.Web.Models
+{
+    public class Entry
+    {
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ TweetJournal.Web/gulpfile.js
@@ -0,0 +1,14 @@
+﻿const { task, src, dest } = require('gulp');
+
+task('css', () => {
+    const postcss = require('gulp-postcss');
+    const postcssPipe = postcss([
+        require('precss'),
+        require('tailwindcss'),
+        require('autoprefixer')
+    ])
+
+    return src('./Styles/main.css')
+        .pipe(postcssPipe)
+        .pipe(dest('./wwwroot/css/'))
+})
