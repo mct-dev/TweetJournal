6b11b7b13eb85ba6bba18c1da58242ea406013e3
Michael Tobias <michael@bitcoinira.com>
2020-08-06 20:41:30 -0700

    add tailwind and gulp

--- TweetJournal.Web/gulpfile.js
+++ TweetJournal.Web/gulpfile.js
@@ -1,14 +1,22 @@
-﻿const { task, src, dest } = require('gulp');
+﻿const { task, src, dest, watch } = require('gulp');
 
-task('css', () => {
-    const postcss = require('gulp-postcss');
-    const postcssPipe = postcss([
-        require('precss'),
-        require('tailwindcss'),
-        require('autoprefixer')
-    ])
+function css() {
+  const postcss = require('gulp-postcss');
+  const postcssPipe = postcss([
+      require('precss'),
+      require('tailwindcss'),
+      require('autoprefixer')
+  ])
 
-    return src('./Styles/main.css')
-        .pipe(postcssPipe)
-        .pipe(dest('./wwwroot/css/'))
+  return src('./Styles/main.css')
+      .pipe(postcssPipe)
+      .pipe(dest('./wwwroot/css/'))
+}
+
+task('css', css)
+
+task('css:watch', () => {
+  watch(['./Styles/**/*.css, ./Styles/*.css'], function() {
+    css()
+  })
 })
